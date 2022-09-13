482a09a683fd1a3c32c2b97ed5600366e9380fc7
Adil Mezghouti <adilmezghouti@gmail.com>
2022-09-13 18:12:43 -0400

    Added some missing files

--- NOAP/contracts/IRelayRecipient.sol
+++ NOAP/contracts/IRelayRecipient.sol
@@ -11,11 +11,7 @@ abstract contract IRelayRecipient {
      * the forwarder is required to verify the sender's signature, and verify
      * the call is not a replay.
      */
-    function isTrustedForwarder(address forwarder)
-        public
-        view
-        virtual
-        returns (bool);
+    function isTrustedForwarder(address forwarder) public view virtual returns (bool);
 
     /**
      * return the sender of this call.
@@ -24,7 +20,7 @@ abstract contract IRelayRecipient {
      * otherwise, return `msg.sender`
      * should be used in the contract anywhere instead of msg.sender
      */
-    function _msgSender() internal view virtual returns (address payable);
+    function _msgSender() internal view virtual returns (address);
 
     function versionRecipient() external view virtual returns (string memory);
 }
